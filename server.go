b0ad8f9a8c87e8a20b56d797161cac8a9822bf47
= <mypochtav@gmail.com>
2021-10-04 17:45:10 +0300

    change port

--- server.go
+++ server.go
@@ -23,5 +23,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/time", handler)
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":8795", nil)
 }
